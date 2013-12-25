c56bd955e701bebf1ad9388f2007b83084ee7d42
joinedapp <arash_hassibi@yahoo.com>
2013-12-25 21:38:41 +0000

    UPDATE: debug output for port number

--- web.js
+++ web.js
@@ -10,5 +10,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on port: " + port);
 });
